9fb814023589386a457c829613f5df0f1ab5a9fa
Andy Jakubowski <hello@andyjakubowski.com>
2020-02-14 09:30:35 +0100

    Tweak 'Set CSS vars in JS' spike

--- set_css_vars_in_js/api.rb
+++ set_css_vars_in_js/api.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 require 'json'
 require 'pry'
-
-NAMES = ['Marcin', 'Danuta', 'Krystyna', 'Magda', 'Maria', 'Kasia']
-nameHash = NAMES.map { |name| { name: name } }
+require 'easydb_io'
 
 colors = {
   'text-primary' => '#071421',
@@ -15,6 +13,11 @@ colors = {
 }
 
 colorsArray = colors.keys.map { |key| { name: key, value: colors[key] } }
+
+db = EasydbIo::EasyDB.new(
+  'f7fca69b-f067-48e8-b7a4-fc192c207fc0',
+  '164c6159-812e-424e-881f-acbfab85fa27'
+)
 
 get '/' do
   headers({ 'Access-Control-Allow-Origin' => '*' })
